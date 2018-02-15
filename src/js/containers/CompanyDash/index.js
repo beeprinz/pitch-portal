73502849cb8546c3dcfd41cd5e2f14a713225da5
Romer Alfonso <romer.alfonso03@gmail.com>
2018-02-15 10:56:06 -0800

    Mapped projects with the projectId through companyDash

--- src/js/containers/CompanyDash/index.js
+++ src/js/containers/CompanyDash/index.js
@@ -6,8 +6,10 @@ function mapStoreToProps(store){
     console.log('Store',store)
     return{
 projectId:store.companyDash.projectId,
-   projectName:store.companyDash.projectName
-       
+   projectName:store.companyDash.projectName,
+   projectStatus:store.companyDash.projectStatus,
+   time:store.companyDash.time,
+       projects: store.companyDash.projects
     };
 }
 
