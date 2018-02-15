3a79d1b0b351f997bd2b32c8aa3e054ce5ff6dbc
Brendan Prinz <brendanprinz@gmail.com>
2018-02-15 11:10:57 -0800

    Authentication for login page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const public = path.join(__dirname, '..', 'public');
 const dist = path.join(__dirname, '..', 'dist');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -22,9 +25,22 @@ app.use(morgan('dev'));
 app.use(express.static(dist));
 app.use(express.static(public));
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(public, 'index.html'));
+app.post('/login', function(req, res) {
+  user.login({
+    email: req.body.email,
+    password: req.body.password
+  }, 'user', function(err, token) {
+    if (err) {
+     console.log(err)
+    } else {
+      console.log(token)
+    }
+  });
 });
+
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(public, 'index.html'));
+// });
 
 module.exports = app;
--- src/js/containers/Login/LoginActions.js
+++ src/js/containers/Login/LoginActions.js
@@ -4,24 +4,26 @@ import axios from 'axios'
     LOG_USER_IN: 'LOG_USER_IN',
     LOG_USER_IN_FAILED: 'LOG_USER_IN_FAILED'
  };
- 
-export function LogUserIn(values) {
+
+ export function LogUserIn(values) {
     console.log('values form actions',values)
       return {
           type: types.LOG_USER_IN,
           payload: axios
-          .post('http://localhost:3000/api/users/login', values)
-          .then(r => {
-              return r.data
-          console.log('actions id payload',r.data)})
+          .post('http://localhost:3000/login', values)
+          .then(response => {
+            console.log(response.data)
+              return response.data //Token
+
+              
+        })
           .catch(err => {
-            console.log(err);
-          }), 
-       
+            console.log('error',err);
+          }),
       }
 }
  export function userError(values){
-     
+
      return{
          type: types.LOG_USER_IN_FAILED,
          payload: values
