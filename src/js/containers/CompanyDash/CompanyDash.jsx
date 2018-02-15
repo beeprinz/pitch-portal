c37f5288a3e88dbd11f28dc5629ea336385c1fda
Miguel Arvizu <30376876+marvizusd@users.noreply.github.com>
2018-02-15 08:51:00 -0800

    finished sprint added hamburger icon for settings drop down

--- src/js/containers/CompanyDash/CompanyDash.jsx
+++ src/js/containers/CompanyDash/CompanyDash.jsx
@@ -62,31 +62,6 @@ export default class CompanyDash extends Component {
     return (
       <div className="container">
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand" href="#">Pitch Portal</a>
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-          </div>
-        </nav>
-
-
         <h1 style={{ marginTop: 50 + "px", marginBottom: 30 + "px" }}>Hello World - CompanyDash</h1>
         <table className="table table-hover ">
           <thead className="thead-dark">
@@ -166,43 +141,7 @@ export default class CompanyDash extends Component {
 
 
 
-        <footer className="pt-4 my-md-5 pt-md-5 border-top">
-          <div className="row">
-            <div className="col-12 col-md">
-              <img className="mb-2" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24" />
-              <small className="d-block mb-3 text-muted">© 2017-2018</small>
-            </div>
-            <div className="col-6 col-md">
-              <h5>Features</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Cool stuff</a></li>
-                <li><a className="text-muted" href="#">Random feature</a></li>
-                <li><a className="text-muted" href="#">Team feature</a></li>
-                <li><a className="text-muted" href="#">Stuff for developers</a></li>
-                <li><a className="text-muted" href="#">Another one</a></li>
-                <li><a className="text-muted" href="#">Last time</a></li>
-              </ul>
-            </div>
-            <div className="col-6 col-md">
-              <h5>Resources</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Resource</a></li>
-                <li><a className="text-muted" href="#">Resource name</a></li>
-                <li><a className="text-muted" href="#">Another resource</a></li>
-                <li><a className="text-muted" href="#">Final resource</a></li>
-              </ul>
-            </div>
-            <div className="col-6 col-md">
-              <h5>About</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Team</a></li>
-                <li><a className="text-muted" href="#">Locations</a></li>
-                <li><a className="text-muted" href="#">Privacy</a></li>
-                <li><a className="text-muted" href="#">Terms</a></li>
-              </ul>
-            </div>
-          </div>
-        </footer>
+
 
       </div>
 
