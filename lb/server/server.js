5a40c48b5ade800710f9d38290ac328c141c78fb
Steven Del Rosario <stevendelro@gmail.com>
2018-02-22 15:55:41 -0800

    npm installed after rebase

--- lb/server/server.js
+++ lb/server/server.js
@@ -24,107 +24,107 @@ app.start = function() {
   });
 };
 
-app.get('/fetchprojects/:userId', (req,res) => {
+app.get('/fetchprojects/:userId', (req, res) => {
   let Projects = app.models.project;
-  console.log(req.params.userId)
+  console.log(req.params.userId);
 
-  Projects.find({where: {userId: req.params.userId}}, function(err, projects) {
-    if(err){
-      res.send(err)
-    }else{
-    res.send(projects)
-    }
-})
-
-app.get('/allProjects', (req, res) => {
-  let Projects = app.models.project;
-  let Users = app.models.user;
-  Projects.find((err, projects) => {
-    if(err){
-      console.log(err)
-      res.send(err)
-    }else{
-      console.log(projects);
-      axios.get('http://localhost:3000/api/users/')
-        .then(users => {
-          return res.send({ users: users.data, projects})
-        });
-      
-    }
-  });
-})
-app.post('/customSignUp', (req, res, next) => {
-  let loginInfo = {
-    email: req.body.email,
-    password: req.body.password
-  };
-  let User = app.models.User;
-
-  User.create(
-    {
-      email: req.body.email,
-      password: req.body.password,
-      phone: req.body.phone,
-      company: req.body.company,
-      website: req.body.website,
-      info: req.body.info,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      position: req.body.position
-    },
-    (err, userInstance) => {
-      if (err) {
-        console.log('Error in User.create: ', err);
-      } else {
-        axios
-          .post('http://localhost:3000/api/users/login', loginInfo)
-          .then(response => res.send(response.data));
-      }
-    }
-  );
-});
-
-app.post('/login', (req, res) => {
-  // User Model Defined
-  let User = app.models.User;
-  let userInfo = req.body;
-  User.login({email: userInfo.email, password: userInfo.password}, function (err, token) {
-    if (err){
-      res.status(404).send('404')
-    }else {
-      res.send(token)
-    }
-  });
-});
-
-
-
-
-// app.get('/getprojects/:userId', (req,res)){
-//   let Project = app.models.projects
-
-
-app.post('/createproject', (req,res) => {
-  // console.log(req.body)
-  // Projects Defined Model
-  let Project = app.models.project;
-  let newProject = req.body
-  Project.create(newProject, (err,project) => {
+  Projects.find({ where: { userId: req.params.userId } }, function(
+    err,
+    projects
+  ) {
     if (err) {
-      res.send(err)
+      res.send(err);
     } else {
-      console.log(project)
-      res.send(project)
+      res.send(projects);
     }
-  })
+  });
 
-})
+  app.get('/allProjects', (req, res) => {
+    let Projects = app.models.project;
+    let Users = app.models.user;
+    Projects.find((err, projects) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        console.log(projects);
+        axios.get('http://localhost:3000/api/users/').then(users => {
+          return res.send({ users: users.data, projects });
+        });
+      }
+    });
+  });
+  app.post('/customSignUp', (req, res, next) => {
+    let loginInfo = {
+      email: req.body.email,
+      password: req.body.password
+    };
+    let User = app.models.User;
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
+    User.create(
+      {
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+        company: req.body.company,
+        website: req.body.website,
+        info: req.body.info,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        position: req.body.position
+      },
+      (err, userInstance) => {
+        if (err) {
+          console.log('Error in User.create: ', err);
+        } else {
+          axios
+            .post('http://localhost:3000/api/users/login', loginInfo)
+            .then(response => res.send(response.data));
+        }
+      }
+    );
+  });
 
-  // start the server if `$ node server.js`
-  if (require.main === module) app.start();
+  app.post('/login', (req, res) => {
+    // User Model Defined
+    let User = app.models.User;
+    let userInfo = req.body;
+    User.login({ email: userInfo.email, password: userInfo.password }, function(
+      err,
+      token
+    ) {
+      if (err) {
+        res.status(404).send('404');
+      } else {
+        res.send(token);
+      }
+    });
+  });
+
+  // app.get('/getprojects/:userId', (req,res)){
+  //   let Project = app.models.projects
+
+  app.post('/createproject', (req, res) => {
+    // console.log(req.body)
+    // Projects Defined Model
+    let Project = app.models.project;
+    let newProject = req.body;
+    Project.create(newProject, (err, project) => {
+      if (err) {
+        res.send(err);
+      } else {
+        console.log(project);
+        res.send(project);
+      }
+    });
+  });
+
+  // Bootstrap the application, configure models, datasources and middleware.
+  // Sub-apps like REST API are mounted via boot scripts.
+  boot(app, __dirname, function(err) {
+    if (err) throw err;
+
+    // start the server if `$ node server.js`
+    if (require.main === module) app.start();
+  });
 });
