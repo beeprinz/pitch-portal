c94f669785022f122b3db2f9610734158d735752
Steven Del Rosario <stevendelro@gmail.com>
2018-02-28 14:24:30 -0800

    Removed  Cookies.Set

--- src/js/containers/SignUp/SignUpActions.js
+++ src/js/containers/SignUp/SignUpActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import Cookies from 'cookies-js';
 
 const USER = '/customSignUp';
 const ROOT_URL = 'http://localhost:3000';
@@ -10,40 +9,37 @@ export const SIGNUP_FULFILLED = 'SIGNUP_FULFILLED';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';
 
 const tokenReceived = response => ({
-	type: TOKEN_RECEIVED,
-	payload: response
+  type: TOKEN_RECEIVED,
+  payload: response
 });
 const signUpData = values => ({
-	type: SIGNUP_FULFILLED,
-	payload: values
+  type: SIGNUP_FULFILLED,
+  payload: values
 });
 const signupError = error => ({
-	type: SIGNUP_ERROR,
-	payload: error
+  type: SIGNUP_ERROR,
+  payload: error
 });
 
 export function signUpNewUser(values) {
-	const url = `${ROOT_URL}${USER}`;
-	//deletes confirmation password from state
-	delete values.password2;
-	return dispatch => {
-		//saves signup data to application state
-		dispatch(signUpData(values));
-		axios
-			//post call to loopback server to signup new user + log them in
-			.post(url, values)
-			// receives access token object and saves it to application state
-			.then(response => {
-				dispatch(tokenReceived(response.data));
-				//Set authToken and userId into cookies
-				Cookies.set('token', response.data.id);
-				Cookies.set('userId', response.data.userId);
-				//Set authToken and userId into sesh storage
-				sessionStorage.setItem('token', response.data.id);
-				sessionStorage.setItem('userId', response.data.userId);
-			})
-			.catch(error => {
-				dispatch(signupError(error));
-			});
-	};
+  const url = `${ROOT_URL}${USER}`;
+  //deletes confirmation password from state
+  delete values.password2;
+  return dispatch => {
+    //saves signup data to application state
+    dispatch(signUpData(values));
+    axios
+      //post call to loopback server to signup new user + log them in
+      .post(url, values)
+      // receives access token object and saves it to application state
+      .then(response => {
+        dispatch(tokenReceived(response.data));
+        //Set authToken and userId into sesh storage
+        sessionStorage.setItem('token', response.data.id);
+        sessionStorage.setItem('userId', response.data.userId);
+      })
+      .catch(error => {
+        dispatch(signupError(error));
+      });
+  };
 }
