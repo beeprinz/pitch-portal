b140d6543d9c9c5609781599eafe8027a0ed7232
Miguel Arvizu <30376876+marvizusd@users.noreply.github.com>
2018-03-01 15:26:16 -0800

    changes

--- src/js/containers/Comments/CommentsActions.js
+++ src/js/containers/Comments/CommentsActions.js
@@ -21,7 +21,6 @@ export function intitialRender(){
     }
 }
 
-
 // export function displayComments(){
 //     const tempUser = '5a8b5f0bb54fb80d00b7cb08'
 //     axios.get(`http://localhost:3000/api/users/${tempUser}/comments`)
