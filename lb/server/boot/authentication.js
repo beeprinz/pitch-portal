dee00fe42fc7bf3edaf54e5bbc2622819db1b0af
Steven Del Rosario <stevendelro@gmail.com>
2018-03-04 14:19:11 -0800

    Anthony made changes to the ACLs for us.

--- lb/server/boot/authentication.js
+++ lb/server/boot/authentication.js
@@ -2,5 +2,11 @@
 
 module.exports = function enableAuthentication(server) {
   // enable authentication
-  // server.enableAuth();
+  //  server.enableAuth();
 };
+
+
+//AWLykCL7BvmpQpjktYMFFZ7ICtGdrY9KcEFoAXhDClJWzJEmeOn6yaFHrEvFLQKe
+
+
+//PruKZWzp32uJCP9sgKikP9QDjci8FHodecR1DPGntHuZEjR9KEXSMipuToyhL8T3
